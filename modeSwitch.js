474bd546c292441f8584508b9fab625199739056
frazern99 <frazern99@gmail.com>
2025-01-09 01:32:31 -0500

    Cookie Add

--- modeSwitch.js
+++ modeSwitch.js
@@ -9,12 +9,28 @@ toggleSwitch.addEventListener('change', boxChecked);
 
 function boxChecked(event){
     if (this.checked) {
-        stylesheet.setAttribute("href", "stylesheetdark.css")
+        stylesheet.setAttribute("href", "stylesheetdark.css");
+        document.cookie = "isDark=true;";
+        let y = document.cookie;
+        console.log(y);
     } else {
-        stylesheet.setAttribute("href", "stylesheetlight.css")
+        stylesheet.setAttribute("href", "stylesheetlight.css");
+        document.cookie = "isDark=false;";
+        let y = document.cookie;
+        console.log(y);
     }
   
 
 
 
  }
+
+ function setDark(){
+    stylesheet.setAttribute("href", "stylesheetdark.css")
+
+ } 
+
+ function setLight(){
+    stylesheet.setAttribute("href", "stylesheetlight.css")
+    
+ } 
